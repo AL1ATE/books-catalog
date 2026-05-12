de169143859e740b6f5853ac6c9596ceef7b24a1
Nikita Obukhov <nikita01141976@mail.ru>
2026-05-12 16:32:29 +0300

    feat: add auth

---
+++ controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\LoginForm;
+use Yii;
+use yii\web\Controller;
+use yii\filters\AccessControl;
+
+class AuthController extends Controller
+{
+    public function behaviors(): array
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goHome();
+        }
+
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+}
