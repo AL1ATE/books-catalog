7330bd05d3c08d248909159f2ce0644f5b491e1d
Nikita Obukhov <nikita01141976@mail.ru>
2026-05-12 16:25:47 +0300

    feat: add migration and settings for config db

--- config/db.php
+++ config/db.php
@@ -1,14 +1,9 @@
 <?php
 
 return [
-    'class' => \yii\db\Connection::class,
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'class' => yii\db\Connection::class,
+    'dsn' => 'mysql:host=mysql;dbname=books_catalog',
+    'username' => 'books_user',
+    'password' => 'books_password',
+    'charset' => 'utf8mb4',
 ];
